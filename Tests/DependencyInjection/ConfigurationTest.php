05018726ef4ab1ca5337d6ad8a67fe2ff5c134a5
Marius Beineris <marius.beineris@activegamingmedia.com>
2018-10-25 13:04:43 +0900

    backward compatability for phpunit

--- Tests/DependencyInjection/ConfigurationTest.php
+++ Tests/DependencyInjection/ConfigurationTest.php
@@ -20,6 +20,11 @@ class ConfigurationTest extends WebTestCase
     public function setUp()
     {
         $this->processor = new Processor();
+
+        if (!class_exists('\PHPUnit\Framework\TestCase') &&
+            class_exists('\PHPUnit_Framework_TestCase')) {
+            class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+        }
     }
 
     /**
